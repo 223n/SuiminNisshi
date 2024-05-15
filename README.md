d187cafd2e64b1a85c973bcdb022bfa059a35745
223n <223n@223n.tech>
2024-05-15 10:47:27 +0900

    Project's Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Capacitor Basic Project
+
+## Running this example
+
+```bash
+yarn install
+npm run dev
+```
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+
+const HOST = process.env.MONACA_SERVER_HOST || '0.0.0.0';
+
+export default defineConfig({
+  root: './src',
+  base: '',
+  build: {
+    outDir: '../www',
+    minify: false,
+    emptyOutDir: false,
+  },
+  server: {
+    host: HOST,
+    port: 8080,
+  },
+  publicDir: '../public'
+});
